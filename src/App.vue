3cb3ab21b9086e67698e8dccbb103e841893794a
amel <mohaddese.computer@gmail.com>
2024-05-06 09:48:20 +0330

    add button for change userName

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <button style="margin-right: 10px" @click="clickf">click number</button>
+    <div class="wrapper">
+      <HelloWorld
+        ref="testg"
+        :userName="userName"
+        :tavak="data"
+        msg="You did it!"
+        @changeData="showData()"
+      />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+<script >
+import { ref } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+export default {
+  components: {
+    TheWelcome,
+    HelloWorld
+  },
+
+  setup(props, { emit }) {
+    console.log('app.js')
+    console.log(props, emit)
+    let userName = ref('mohaddese')
+    let data = ref(2503)
+    function clickf() {
+      return (data.value = 2)
+    }
+    const showData = () => {
+      console.log('cccccccccccccccccccccccccccccccccccccccc')
+      return (userName.value = 'moha Amel')
+    }
+    return {
+      data,
+      clickf,
+      userName,
+      showData
+    }
+  }
+}
+</script>
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,76 @@
+
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h1 class="green">{{ prp }}</h1>
+    <h1>{{ tavak }}</h1>
+    <button @click="testAlert">test</button>
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+    </h3>
+    <button @click="changeUser()">ChangeUserName</button>
+    <h1>{{ userName }}</h1>
+  </div>
+</template>
+<script >
+import { ref, watch } from 'vue'
+export default {
+  props: {
+    tavak: { default: null },
+    msg: { default: null },
+    userName: { type: String }
+  },
+  emits: ['tempo', 'changeData'],
+  setup(props, { emit }) {
+    // console.log(emit)
+    // console.log('props in hello world')
+    // console.log(props)
+    console.log('tavaktavaktavaktavaktavak', props.tavak)
+    let item = ref(null)
+    const temp = ref('hossein')
+    const prp = ref(null)
+    prp.value = props.tavak
+    async function testAlert() {}
+
+    const changeUser = () => {
+      item.value = props.userName
+      emit('changeData')
+    }
+
+    // watch(item, (oldValue, newValue) => {
+    //   console.log('old value', oldValue)
+    //   console.log('new value', newValue)
+    //   changeUser()
+    // })
+
+    return { testAlert, temp, prp, changeUser, item }
+  }
+}
+</script>
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
